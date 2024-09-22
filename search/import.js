f06e4a7dbe1e629eb37c8136b505cfd660805b22
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-09-22 21:32:23 +0900

    テキストをインポートするjsを追加

---
+++ search/import.js
@@ -0,0 +1,6 @@
+// path=テキストファイルのパス
+const url = new URL(import.meta.url);
+const path = url.searchParams.get("path");
+const response = await fetch(path);
+const text = await response.text();
+export default text;
